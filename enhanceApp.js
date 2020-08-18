2d048237f8e28551981de6f7f2e65a139ab22df1
Stuart Clark <stuart@realityloop.com>
2020-08-18 15:48:34 +1000

    [#1] Updated dependencies.

--- enhanceApp.js
+++ enhanceApp.js
@@ -1,39 +1,22 @@
 import Vuex from 'vuex'
+import { DruxtRouterStore } from 'druxt-router'
+import { DruxtSchemaStore } from 'druxt-schema'
 
-/**
- * Import module.
- *
- * @param {string} module - The module to be imported.
- * @param {object} siteData - The Vuepress site configuration.
- */
-const importModule = (module, siteData) => {
-  if (siteData.base.indexOf(module) === 1) {
-    return false
-  }
-
-  return import('druxt-router').then(m => m.DruxtRouterStore)
-}
-
-export default ({ Vue, options, router, siteData }) => {
+export default async ({ Vue, options, router, siteData }) => {
   // Setup Vuex.
   Vue.use(Vuex)
   const store = new Vuex.Store({})
 
   // Setup Druxt.js Router.
-  const DruxtRouterStore = importModule('druxt-router', siteData)
-  if (DruxtRouterStore) {
-    store.$druxtRouter = () => ({
-      getResource: async (query = {}) => require(`./data/resources/${query.type}/${query.id}.json`)
-    })
-  }
+  DruxtRouterStore({ store })
+  store.$druxtRouter = () => ({
+    getResource: async (query = {}) => require(`./data/resources/${query.type}/${query.id}.json`)
+  })
 
   // Setup Druxt.js Schema.
-  const DruxtSchemaStore = importModule('druxt-schema', siteData)
-  if (DruxtSchemaStore) {
-    DruxtSchemaStore({ store })
-    store.$druxtSchema = {
-      import: async id => require(`./data/schemas/${id}.json`)
-    }
+  DruxtSchemaStore({ store })
+  store.$druxtSchema = {
+    import: async id => require(`./data/schemas/${id}.json`)
   }
 
   Vue.mixin({ store })
